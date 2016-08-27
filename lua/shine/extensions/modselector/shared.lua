375d99f97e3f94fb6699d5acc04e73c4d7eeab62
keatsandyeats <keatsandyeats@users.noreply.github.com>
2016-08-27 03:22:07 -0400

    Turn block comments into many single-line comments
    luajit parser doesn't like those apparently

--- lua/shine/extensions/modselector/shared.lua
+++ lua/shine/extensions/modselector/shared.lua
@@ -1,13 +1,13 @@
 --[[
 	A Shine plugin to enable and disable server mods in-game.
-	
-	The config will have a list of mod hexes, each with a string and a boolean attribute.
-	The config will pull mods from MapCycle.json or mods can be manually added to the config.
-	The config list is just so shine's gui has something to pull from.
-	Manually editing true/false will be overwritten at plugin load when it reads from MapCycle.json.
-	TODO: The command will be hooked up to Shine's admin menu.
-	TODO: remove boolean from config as it's useless for the end user
 --]]
+	-- The config will have a list of mod hexes, each with a string and a boolean attribute.
+	-- The config will pull mods from MapCycle.json or mods can be manually added to the config.
+	-- The config list is just so shine's gui has something to pull from.
+	-- Manually editing true/false will be overwritten at plugin load when it reads from MapCycle.json.
+	-- TODO: The command will be hooked up to Shine's admin menu.
+	-- TODO: remove boolean from config as it's useless for the end user
+
 
 local Shine = Shine
 local Plugin = {}
